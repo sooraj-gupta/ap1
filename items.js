019603987610ef62bd15243fd71fc32664c3cc5f
Sooraj Gupta <soorajgupta04@gmail.com>
2020-11-23 15:13:58 -0800

    m

--- items.js
+++ items.js
@@ -1,4 +1,4 @@
-var items = ' [ { "type": "d", "name": "Dynamics", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Dynamics Study Guide", "link":"dynamics.pdf" } ] } ] }, { "type": "d", "name": "Circular Motion and Gravity", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Circular Motion and Gravity Study Guide", "link":"Circularmotionandgravity.pdf" } ] } ] }, { "type": "d", "name": "Energy", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Energy Study Guide", "link":"energy.png" } ] } ] } ] ';
+var items = ' [ { "type": "d", "name": "Dynamics", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Dynamics Study Guide", "link":"dynamics.pdf" } ] } ] }, { "type": "d", "name": "Circular Motion and Gravity", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Circular Motion and Gravity Study Guide", "link":"Circularmotionandgravity.pdf" } ] } ] }, { "type": "d", "name": "Energy", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Energy Study Guide", "link":"energy.png" } ] }, { "type":"d", "name":"Test", "content":[ { "type": "f", "name": "Energy 2020 Test", "link":"energytest.png" } ] } ] } ] ';
 
 
 
@@ -68,7 +68,19 @@ var items =
 						"link":"energy.png" 
 					}
 				]
+			},
+			{
+				"type":"d",
+				"name":"Test",
+				"content":[
+					{ 
+						"type": "f", 
+						"name": "Energy 2020 Test", 
+						"link":"energytest.png" 
+					}
+				]
 			}
+			
 		]
 	}
 ] 
