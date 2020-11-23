c5b3066cea8778d83c3712f01ec34c70d03b96ca
Sooraj Gupta <soorajgupta04@gmail.com>
2020-11-22 23:30:09 -0800

    m

---
+++ items.js
@@ -0,0 +1,77 @@
+var items = ' [ { "type": "d", "name": "Dynamics", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Dynamics Study Guide", "link":"dynamics.pdf" } ] } ] }, { "type": "d", "name": "Circular Motion and Gravity", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Circular Motion and Gravity Study Guide", "link":"Circularmotionandgravity.pdf" } ] } ] }, { "type": "d", "name": "Energy", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Energy Study Guide", "link":"energy.png" } ] } ] } ] ';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var items =
+'
+[
+	{
+		"type": "d",
+		"name": "Dynamics",
+		"content": [
+			{
+				"type":"d",
+				"name":"Study Guides",
+				"content":[
+					{ 
+						"type": "f", 
+						"name": "Dynamics Study Guide", 
+						"link":"dynamics.pdf" 
+					}
+				]
+			}
+		]
+	},
+	{
+		"type": "d",
+		"name": "Circular Motion and Gravity",
+		"content": [
+			{
+				"type":"d",
+				"name":"Study Guides",
+				"content":[
+					{ 
+						"type": "f", 
+						"name": "Circular Motion and Gravity Study Guide", 
+						"link":"Circularmotionandgravity.pdf" 
+					}
+				]
+			}
+		]
+	},
+	{
+		"type": "d",
+		"name": "Energy",
+		"content": [
+			{
+				"type":"d",
+				"name":"Study Guides",
+				"content":[
+					{ 
+						"type": "f", 
+						"name": "Energy Study Guide", 
+						"link":"energy.png" 
+					}
+				]
+			}
+		]
+	}
+] 
+';
+
+&nbsp; <a class = link href = web/socialmediaanimation/index.html>View Webpage</a>*/
