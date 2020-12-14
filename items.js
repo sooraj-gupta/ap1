d940e85dbffeb342caff558efa9ce73ffc68b989
sooraj-gupta <soorajgupta04@gmail.com>
2020-12-14 13:00:59 -0800

    M

--- items.js
+++ items.js
@@ -1,8 +1,4 @@
-var items = ' [ { "type": "d", "name": "Kinematics 1D/2D ", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Kinematics Study Guide", "link":"files/kinematics/studyguides/kinematics.png" }, { "type": "f", "name": "Kinematics Study Guide ( pdf )", "link":"files/kinematics/studyguides/kinematics.pdf" } ] } ] }, { "type": "d", "name": "Dynamics", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Dynamics Study Guide", "link":"files/dynamics/studyguides/dynamics.png" }, { "type": "f", "name": "Dynamics Study Guide ( pdf )", "link":"files/dynamics/studyguides/dynamics.pdf" } ] } ] }, { "type": "d", "name": "Circular Motion and Gravity", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Circular Motion and Gravity Study Guide", "link":"files/ircularmotionandgravity/studyguides/Circularmotionandgravity.pdf" } ] } ] }, { "type": "d", "name": "Energy", "content": [ { "type":"d", "name":"Notes", "content":[ { "type": "f", "name": "Energy Day 3 Notes", "link":"files/energy/notes/energyday3.png" } ] }, { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Energy Study Guide", "link":"files/energy/studyguides/energy.png" } ] }, { "type":"d", "name":"Test", "content":[ { "type": "f", "name": "Energy 2020 Test", "link":"files/energy/test/energytest.png" } ] } ] }, { "type": "d", "name": "Linear Momentum", "content": [ { "type":"d", "name":"Notes", "content":[ ] }, { "type":"d", "name":"Study Guides", "content":[ { } ] }, { "type":"d", "name":"Test", "content":[ { } ] } ] } ] ';
-
-
-
-
+var items = ' [ { "type": "d", "name": "Kinematics 1D/2D ", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Kinematics Study Guide", "link":"files/kinematics/studyguides/kinematics.png" }, { "type": "f", "name": "Kinematics Study Guide ( pdf )", "link":"files/kinematics/studyguides/kinematics.pdf" } ] } ] }, { "type": "d", "name": "Dynamics", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Dynamics Study Guide", "link":"files/dynamics/studyguides/dynamics.png" }, { "type": "f", "name": "Dynamics Study Guide ( pdf )", "link":"files/dynamics/studyguides/dynamics.pdf" } ] } ] }, { "type": "d", "name": "Circular Motion and Gravity", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Circular Motion and Gravity Study Guide", "link":"files/ircularmotionandgravity/studyguides/Circularmotionandgravity.pdf" } ] } ] }, { "type": "d", "name": "Energy", "content": [ { "type":"d", "name":"Notes", "content":[ { "type": "f", "name": "Energy Day 3 Notes", "link":"files/energy/notes/energyday3.png" } ] }, { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Energy Study Guide", "link":"files/energy/studyguides/energy.png" } ] }, { "type":"d", "name":"Test", "content":[ { "type": "f", "name": "Energy 2020 Test", "link":"files/energy/test/energytest.png" } ] } ] }, { "type": "d", "name": "Linear Momentum", "content": [ { "type":"d", "name":"Test", "content":[ { "type":"f", "name":"Linear Momentum 2020 Test", "link":"files/midterm/test/midtermexam.png" } ] } ] }, { "type": "d", "name": "Midterm", "content": [ { "type":"d", "name":"Test", "content":[ { "type":"f", "name":"Midterm 2020 Test", "link":"files/midterm/test/midtermexam.png" } ] } ] } ] ';
 
 
 
@@ -123,30 +119,32 @@ var items =
 		"content": [
 			{
 				"type":"d",
-				"name":"Notes",
-				"content":[
-					
-				]
-			},
-			{
-				"type":"d",
-				"name":"Study Guides",
+				"name":"Test",
 				"content":[
 					{ 
-						
+						"type":"f",
+						"name":"Linear Momentum 2020 Test",
+						"link":"files/midterm/test/midtermexam.png"
 					}
 				]
-			},
+			}
+		]
+	},
+	{
+		"type": "d",
+		"name": "Midterm",
+		"content": [
 			{
 				"type":"d",
 				"name":"Test",
 				"content":[
 					{ 
-						
+						"type":"f",
+						"name":"Midterm 2020 Test",
+						"link":"files/midterm/test/midtermexam.png"
 					}
 				]
 			}
-			
 		]
 	}
 ] 
