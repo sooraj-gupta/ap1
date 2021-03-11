5f852728489fa999b6108f7b6dc62a2f0bf8f495
sooraj-gupta <soorajgupta04@gmail.com>
2021-03-10 21:32:55 -0800

    added shm

--- items.js
+++ items.js
@@ -1,4 +1,4 @@
-var items = ' [ { "type": "d", "name": "Kinematics 1D/2D ", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Kinematics Study Guide", "link":"files/kinematics/studyguides/kinematics.png" }, { "type": "f", "name": "Kinematics Study Guide ( pdf )", "link":"files/kinematics/studyguides/kinematics.pdf" } ] } ] }, { "type": "d", "name": "Dynamics", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Dynamics Study Guide", "link":"files/dynamics/studyguides/dynamics.png" }, { "type": "f", "name": "Dynamics Study Guide ( pdf )", "link":"files/dynamics/studyguides/dynamics.pdf" } ] } ] }, { "type": "d", "name": "Circular Motion and Gravity", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Circular Motion and Gravity Study Guide", "link":"files/ircularmotionandgravity/studyguides/Circularmotionandgravity.pdf" } ] } ] }, { "type": "d", "name": "Energy", "content": [ { "type":"d", "name":"Notes", "content":[ { "type": "f", "name": "Energy Day 3 Notes", "link":"files/energy/notes/energyday3.png" } ] }, { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Energy Study Guide", "link":"files/energy/studyguides/energy.png" } ] }, { "type":"d", "name":"Test", "content":[ { "type": "f", "name": "Energy 2020 Test", "link":"files/energy/test/energytest.png" } ] } ] }, { "type": "d", "name": "Linear Momentum", "content": [ { "type":"d", "name":"Test", "content":[ { "type":"f", "name":"Linear Momentum 2020 Test", "link":"files/midterm/test/midtermexam.png" } ] } ] }, { "type": "d", "name": "Midterm", "content": [ { "type":"d", "name":"Test", "content":[ { "type":"f", "name":"Midterm 2020 Test", "link":"files/midterm/test/midtermexam.png" } ] } ] }, { "type": "d", "name": "Rotational Kinematics", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type":"f", "name":"Rotational Study Guide", "link":"files/rotationalkinematics/studyguides/rotationalkinematics.pdf" } ] } ] }, { "type": "d", "name": "Rotational Energy and Angular Momentum", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type":"f", "name":"Rotational Energy and Angular Momentum Study Guide", "link":"files/rotationalenergyandangularmomentum/studyguides/rotationalenergyandangularmomentum.pdf" } ] }, { "type":"d", "name":"Test", "content":[ { "type":"f", "name":"Rotational Energy and Angular Momentum Test", "link":"files/rotationalenergyandangularmomentum/test/rotationalenergyandangularmomentumtest.png" } ] } ] } ] ';
+//var items = ' [ { "type": "d", "name": "Kinematics 1D/2D ", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Kinematics Study Guide", "link":"files/kinematics/studyguides/kinematics.png" }, { "type": "f", "name": "Kinematics Study Guide ( pdf )", "link":"files/kinematics/studyguides/kinematics.pdf" } ] } ] }, { "type": "d", "name": "Dynamics", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Dynamics Study Guide", "link":"files/dynamics/studyguides/dynamics.png" }, { "type": "f", "name": "Dynamics Study Guide ( pdf )", "link":"files/dynamics/studyguides/dynamics.pdf" } ] } ] }, { "type": "d", "name": "Circular Motion and Gravity", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Circular Motion and Gravity Study Guide", "link":"files/ircularmotionandgravity/studyguides/Circularmotionandgravity.pdf" } ] } ] }, { "type": "d", "name": "Energy", "content": [ { "type":"d", "name":"Notes", "content":[ { "type": "f", "name": "Energy Day 3 Notes", "link":"files/energy/notes/energyday3.png" } ] }, { "type":"d", "name":"Study Guides", "content":[ { "type": "f", "name": "Energy Study Guide", "link":"files/energy/studyguides/energy.png" } ] }, { "type":"d", "name":"Test", "content":[ { "type": "f", "name": "Energy 2020 Test", "link":"files/energy/test/energytest.png" } ] } ] }, { "type": "d", "name": "Linear Momentum", "content": [ { "type":"d", "name":"Test", "content":[ { "type":"f", "name":"Linear Momentum 2020 Test", "link":"files/midterm/test/midtermexam.png" } ] } ] }, { "type": "d", "name": "Midterm", "content": [ { "type":"d", "name":"Test", "content":[ { "type":"f", "name":"Midterm 2020 Test", "link":"files/midterm/test/midtermexam.png" } ] } ] }, { "type": "d", "name": "Rotational Kinematics", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type":"f", "name":"Rotational Study Guide", "link":"files/rotationalkinematics/studyguides/rotationalkinematics.pdf" } ] } ] }, { "type": "d", "name": "Rotational Energy and Angular Momentum", "content": [ { "type":"d", "name":"Study Guides", "content":[ { "type":"f", "name":"Rotational Energy and Angular Momentum Study Guide", "link":"files/rotationalenergyandangularmomentum/studyguides/rotationalenergyandangularmomentum.pdf" } ] }, { "type":"d", "name":"Test", "content":[ { "type":"f", "name":"Rotational Energy and Angular Momentum Test", "link":"files/rotationalenergyandangularmomentum/test/rotationalenergyandangularmomentumtest.png" } ] } ] } ] ';
 
 
 
@@ -6,9 +6,9 @@ var items = ' [ { "type": "d", "name": "Kinematics 1D/2D ", "content": [ { "type
 
 
 
-/*
+
 var items =
-'
+`
 [
 	{
 		"type": "d",
@@ -189,8 +189,23 @@ var items =
 				]
 			}
 		]
+	},
+	{
+		"type": "d",
+		"name": "Simple Harmonic Motion",
+		"content": [
+			{
+				"type":"d",
+				"name":"Study Guides",
+				"content":[
+					{ 
+						"type":"f",
+						"name":"Simple Harmonic Motion Study Guide",
+						"link":"files/simpleharmonicmotion/studyguides/simpleharmonicmotion.pdf"
+					}
+				]
+			}
+		]
 	}
 ] 
-';
-
-&nbsp; <a class = link href = web/socialmediaanimation/index.html>View Webpage</a>*/
+`
